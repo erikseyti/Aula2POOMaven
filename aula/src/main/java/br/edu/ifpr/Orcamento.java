8dd480ac70d71b593a2b1a53e4a03b78a92d74b8
erikseyti <sey_ti@hotmail.com>
2017-09-06 20:42:53 -0300

    Commit de Teste

--- aula/src/main/java/br/edu/ifpr/Orcamento.java
+++ aula/src/main/java/br/edu/ifpr/Orcamento.java
@@ -1,5 +1,21 @@
 package br.edu.ifpr;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Orcamento {
+	
+	private Collection<Item> itens = new ArrayList<Item>();
+	
+	
+	public void adicionar(Item item)
+	{
+		itens.add(item);
+	}
+	
+	public void remover(int indice)
+	{
+		System.out.println("hello");
+	}
 
 }
